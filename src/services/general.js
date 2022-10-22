56ce75e5e293321643233ac714fa14502edde377
Daniel Kwan <danielkwan2000@gmail.com>
2022-10-22 17:21:08 -0400

    updates

--- src/services/general.js
+++ src/services/general.js
@@ -8,7 +8,13 @@ const BASE_URI = 'https://pacific-eyrie-13165.herokuapp.com/'
 export const cargoServices = {
     async getAllCompanies(companyId = {}){
         return axios.get(`${BASE_URI}/companies/`)
+    },
+
+
+    getCompany(companyId = {}){
+        return axios.get(`${BASE_URI}/companies/${companyId}`)
     }
+
 
 }
 
