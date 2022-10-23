aef8cd594c061fc5e1c3717dd74a051c8269da33
harshul25 <harshul.mehta863@gmail.com>
2022-10-23 12:05:08 -0400

    made a table

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css';
 
 import NavBar from './components/navbar';
 import FillForm from './components/fillableForm';
+import BasicTable from './components/tablescript';
 function App() {
   return (
     <>
     <NavBar/>
     <FillForm/>
+    <BasicTable/>
     </>
   );
 }
